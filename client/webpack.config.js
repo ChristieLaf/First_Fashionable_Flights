c875e72cee778a406ac5a7a349c69f67cd9fefa8
Christie Laferriere <christielaferriere@caitlins-mbp.mynetworksettings.com>
2024-07-14 23:48:53 -0400

    updating flightsAPi key b/c old one was uploaded to Github, new api key is been stored securely in special file, replacing ReactDOM usage with createRoot method since ReactDOM is no longer uspported

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,69 +1,69 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 module.exports = {
-    entry: './client/src/index.js',
-    // resolve: {
-    //     extensions: ['.js', '.jsx', '.json', '.wasm'] // Add '.jsx' if you are using React JSX
-    // },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
+  entry: './client/src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
 
-    module: {
-        //if i wnat to add images, i have to add another rules
-        rules: [
-            {// join zoom .....
-                test: /\.js$/, 
-                exclude: /node_modules/, 
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-react', '@babel/preset-env'], 
-                        plugins: ['@babel/plugin-transform-runtime'] 
-                    }
-                }
-            },
-            {
-                test: /\.css$/, 
-                use: ['style-loader', 'css-loader'] 
-            }
-        ]
-    },
-    // Plugins configuration
-    plugins: [ //naviagte one folder up to the template directory, the base folder is webpack config 
-        new HtmlWebpackPlugin({
-            template: 'template/index.html',
-            filename: 'index.html'
-        })
-    ],
-    // Development server configuration
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'), 
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
         },
-        compress: true, 
-        port: 8080, 
-        open: true,
-        hot: true, 
-        historyApiFallback: true,
-        // proxy: {
-        //     '/api': {
-        //         target: 'http://localhost:3000', 
-        //         secure: false, 
-        //         changeOrigin: true, 
-        //         pathRewrite: { '^/api': '' }, 
-        //     }
-        // }
-        proxy: [
-            {
-              context: ['/api'],
-              changeOrigin: true,
-              secure: false, 
-              target: `http://localhost:3000/`,
-            }
-        ]
-    }
-}
-// enable terminal for me
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  // Plugins configuration
+  plugins: [
+    //naviagte one folder up to the template directory, the base folder is webpack config
+    new HtmlWebpackPlugin({
+      template: 'template/index.html',
+      filename: 'index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_FLIGHT_URL': JSON.stringify(
+        process.env.REACT_APP_FLIGHT_URL
+      ),
+      'process.env.REACT_APP_FLIGHT_API': JSON.stringify(
+        process.env.REACT_APP_FLIGHT_KEY
+      ),
+    }),
+  ],
+  // Development server configuration
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+    proxy: [
+      {
+        context: ['/api'],
+        changeOrigin: true,
+        secure: false,
+        target: `http://localhost:3000/`,
+      },
+    ],
+  },
+};
--- server/controllers/flightController.js
+++ server/controllers/flightController.js
@@ -1,29 +1,24 @@
 const FlightsApi = {
-  "flightUrl": 'https://api.aviationstack.com/v1/flights',
-  "flightKey": 'c0d0dfa7da4489f4d251c414bd2aef04'
+  flightUrl: process.env.REACT_APP_FLIGHT_URL,
+  flightKey: process.env.REACT_APP_FLIGHT_KEY,
 };
 
 const flightController = {
   fetchFlights: async (req, res, next) => {
-        console.log("Reached flightController")
-      const query = `${FlightsApi.flightUrl}?access_key=${FlightsApi.flightKey}`;
+    console.log('Reached flightController');
+    const query = `${FlightsApi.flightUrl}?access_key=${FlightsApi.flightKey}`;
 
-      try {
+    try {
+      const response = await fetch(query);
+      const data = await response.json();
+      req.dataVault = data;
 
-          const response = await fetch(query);
-          const data = await response.json();
-          req.dataVault = data;
-
-         return next(); 
-
-      } catch (error) {
-
-          console.error('Error fetching flights:', error);
-          res.status(500).send('Error fetching flights');
-
-      }
-
-  }
+      return next();
+    } catch (error) {
+      console.error('Error fetching flights:', error);
+      res.status(500).send('Error fetching flights');
+    }
+  },
 };
 
 module.exports = flightController;
